06f6340723c521f2e115d80aba682c595a8390aa
Obed Paz <lester.paz@cit.hn>
2022-07-28 15:56:43 -0600

    Add animations

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,27 @@
-import styles from './layout.module.scss';
+import { motion } from "framer-motion"
 
-const name = 'Obed Paz';
-export const siteTitle = 'Next.js Sample Website';
+import styles from './layout.module.scss';
+import Header from '../Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const variants = {
+    hidden: { opacity: 0, x: -200, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: -100 },
+  }
+
   return (
     <div className={styles.container}>
-      {children}
+      <Header />
+      <motion.main
+        variants={variants}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        transition={{ type: 'linear' }}
+      >
+        {children}
+      </motion.main>
     </div>
   );
 }
