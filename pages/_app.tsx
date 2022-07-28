cc3465a03fa8b9d8d239852f1864e18de4f15ac8
Obed Paz <lester.paz@cit.hn>
2022-07-28 15:56:24 -0600

    Update de App global component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { AnimatePresence } from 'framer-motion'
+
+import '@/styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Creative Information Technologies</title>
+        <meta name="description" content="CIT Landing" />
+      </Head>
+      {/* <Header /> */}
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        {/* canonical={url} key={url} */}
+        <Component {...pageProps} />
+      </AnimatePresence>
+      {/* <Footer /> */}
+    </>
+  )
 }
 
 export default MyApp
