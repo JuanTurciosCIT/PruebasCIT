4bc065ffb10ddd7f83a6da2f27b3600026acd631
Obed Paz <lester.paz@cit.hn>
2022-08-01 14:45:26 -0600

    Add the About component to the Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import HeroHome from './components/Home/HeroHome'
 import CompaniesSlider from './components/Home/CompaniesSlider'
 
 import Layout from '../components/Layout'
+import styles from '../styles/home.module.scss';
+import About from './components/Home/About';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +13,9 @@ const Home: NextPage = () => {
       <main>
         <HeroHome />
         <CompaniesSlider />
+        <div className={styles.aboutAndServices}>
+          <About />
+        </div>
       </main>
     </Layout>
   )
