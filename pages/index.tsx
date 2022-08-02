484ec75d86fa88bea3d140e69bafdb4d0c66e35c
Obed Paz <lester.paz@cit.hn>
2022-08-02 10:46:19 -0600

    Add OurServices component to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,19 @@ import CompaniesSlider from './components/Home/CompaniesSlider'
 import Layout from '../components/Layout'
 import styles from '../styles/home.module.scss';
 import About from './components/Home/About';
+import OurServices from './components/Home/OurServices';
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <main>
         <HeroHome />
-        <CompaniesSlider />
-        <div className={styles.aboutAndServices}>
-          <About />
-        </div>
       </main>
+      <CompaniesSlider />
+      <section className={styles.aboutAndServices}>
+        <About />
+        <OurServices />
+      </section>
     </Layout>
   )
 }
