442dab5c6c90feeeeb70dc0b49d8de0859db69aa
Milton Guevara <milton.guevara@cit.hn>
2022-08-05 14:13:31 -0600

    Add home hero component

---
+++ pages/components/Home/HeroFooter/index.tsx
@@ -0,0 +1,13 @@
+import utils from 'styles/utils.module.scss';
+import styles from './herofooter.module.scss';
+
+export default function HeroFooter() {
+    return <section className={styles.heroFooter}>
+        <div className={styles.wrapper}>
+            <h1 className={`${styles.title} ${utils.headingLarge}` }>Ready to get started?</h1>
+            <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus eu sit suspendisse aliquet arcu bibendum. Turpis commodo libero vulputate sed. Sagittis et, euismod sagittis, leo commodo, a amet. Metus felis ipsum feugiat.</p>
+            <button className={styles.button}>Contact Us</button>
+        </div>
+    </section>
+
+}
