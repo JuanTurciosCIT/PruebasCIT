1083abdd0dc3a4e1bb2115415fcfe84b33c22447
Obed Paz <lester.paz@cit.hn>
2022-08-06 11:56:01 -0600

    Format and refactor code

--- shared/Layout/index.tsx
+++ shared/Layout/index.tsx
@@ -1,28 +1,28 @@
-import { motion } from "framer-motion"
+import { motion } from 'framer-motion';
+import Footer from '../Footer';
 
-import styles from './layout.module.scss';
 import Header from '../Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const variants = {
-    hidden: { opacity: 0, x: -200, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -100 },
-  }
+	const variants = {
+		hidden: { opacity: 0, x: -200, y: 0 },
+		enter: { opacity: 1, x: 0, y: 0 },
+		exit: { opacity: 0, x: 0, y: -100 },
+	};
 
-  return (
-    <div className={styles.container}>
-      <Header />
-      <motion.main
-        variants={variants}
-        initial="hidden"
-        animate="enter"
-        exit="exit"
-        transition={{ type: 'linear' }}
-      >
-        {children}
-      </motion.main>
-      {/* <Footer /> */}
-    </div>
-  );
+	return (
+		<>
+			<Header />
+			<motion.main
+				variants={variants}
+				initial='hidden'
+				animate='enter'
+				exit='exit'
+				transition={{ type: 'linear' }}
+			>
+				{children}
+			</motion.main>
+			<Footer />
+		</>
+	);
 }
