479f64159cb49d94ce2230ae3606812e8bf7117d
Obed Paz <lester.paz@cit.hn>
2022-08-08 11:09:10 -0600

    Create shared Footer component

--- shared/Footer/index.tsx
+++ shared/Footer/index.tsx
@@ -3,6 +3,11 @@ import Image from 'next/image';
 import creativeLogo from '@/images/creative_logo.png';
 import styles from './footer.module.scss';
 import utils from '@/styles/utils.module.scss';
+import instagramIcon from '../../public/svg/instagram_icon.svg';
+import linkedinIcon from '../../public/svg/linkedin_icon.svg';
+import facebookIcon from '../../public/svg/facebook_icon.svg';
+import mailIcon from '../../public/svg/mail_icon.svg';
+import locationIcon from '../../public/svg/location_icon.svg';
 
 // Change all the default export (components) to a named export
 // This is to avoid the error:
@@ -12,22 +17,46 @@ import utils from '@/styles/utils.module.scss';
 
 export default function Footer() {
 	return (
-		<footer>
-			<div className={styles.infoWrapper}>
-				<div>
-					<Image src={creativeLogo} alt='CIT Logo' />
+		<footer className={styles.footer}>
+			<div className={styles.container}>
+				<div className={styles.infoWrapper}>
+					<div className={styles.logo}>
+						<Image src={creativeLogo} alt='CIT Logo' />
+					</div>
+					<h3 className={`${utils.headingSmall} ${styles.subtitle}`}>
+						Locations
+					</h3>
+					<div>
+						<p className={`${utils.textSmall} ${styles.text}`}>
+							Honduras <br />Seguros del Pais 7th floor, aside City Mall, San Pedro
+							Sula, Cortés, Honduras
+						</p>
+						<p className={`${utils.textSmall} ${styles.text}`}>Delaware: <br />City Mall, San Pedro Sula, Cortés, Honduras</p>
+						<p className={`${utils.textSmall} ${styles.text}`}>Estonia: <br />Sepapaja 6 Tallinn 15551</p>
+					</div>
 				</div>
-				<h3 className={`${utils.headingSmall} ${styles.subtitle}`}>
-					Locations
-				</h3>
-				<div>
-					<p className={`${utils.textSmall} ${styles.text}`}>
-						Honduras Seguros del Pais 7th floor, aside City Mall, San Pedro
-						Sula, Cortés, Honduras
-					</p>
-					<p className={`${utils.textSmall} ${styles.text}`}>Delaware: City Mall, San Pedro Sula, Cortés, Honduras</p>
-					<p className={`${utils.textSmall} ${styles.text}`}>Estonia: Sepapaja 6 Tallinn 15551</p>
+				<div className={styles.socialMedia}>
+					<h3 className={`${utils.headingSmall} ${styles.subtitle}`}>
+						Follow us
+					</h3>
+					<div className={styles.mediaIcons}>
+						<div className={styles.iconContainer}>
+							<Image src={instagramIcon} alt='Instagram' width={12} height={12} />
+						</div>
+						<div className={styles.iconContainer}>
+							<Image src={linkedinIcon} alt='LinkedIn' width={12} height={12} />
+						</div>
+						<div className={styles.iconContainer}>
+							<Image src={facebookIcon} alt='Facebook' width={12} height={12} />
+						</div>
+					</div>
+					<p className={`${utils.textSmall} ${styles.contact}`}><Image src={mailIcon}  alt='Mail icon' ></Image> info@cit.hn</p>
+					<p className={`${utils.textSmall} ${styles.contact}`}><Image src={locationIcon}  alt='Mail icon' ></Image> +504 2566-3649</p>
 				</div>
+			</div>
+			<div className={styles.rightsConditions}>
+				<p className={utils.textSmall}>All Rights reservd 2022 yalo</p>
+				<p className={utils.textSmall}>Privacy Policy</p>
 			</div>
 		</footer>
 	);
