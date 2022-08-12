ae3f23132626f850b729ad5be7eb741f70335109
Obed Paz <lester.paz@cit.hn>
2022-08-12 16:30:07 -0600

    Get home data using getStaticProps for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage, InferGetStaticPropsType } from 'next';
 
-import HeroHome from 'components/Home/HeroHome'
-import CompaniesSlider from 'components/Home/CompaniesSlider'
+import HeroHome from 'components/Home/HeroHome';
+import CompaniesSlider from 'components/Home/CompaniesSlider';
 
-import Layout from 'shared/Layout'
+import Layout from 'shared/Layout';
 import styles from 'styles/home.module.scss';
 import About from 'components/Home/About';
 import OurServices from 'components/Home/OurServices';
@@ -12,26 +12,47 @@ import Portfolio from 'components/Home/Portfolio';
 import CustomerFeedback from 'components/Home/CustomerFeedback';
 import Gallery from 'components/Home/Gallery';
 import HeroFooter from 'shared/HeroFooter';
+import { HomeContent } from 'utils/types/homeContent.interface';
+import { getHomeContent } from 'utils/services/getHomeContent';
 
-const Home: NextPage = () => {
-  return (
-    <Layout>
-      <main>
-        <HeroHome />
-      </main>
-      <CompaniesSlider />
-      <div className={styles.aboutAndServices}>
-        <About />
-        <OurServices />
-        <div className={styles.diamond}></div>
-      </div>
-      <Technologies />
-      <Portfolio />
-      <CustomerFeedback />
-      <Gallery />
-      <HeroFooter />
-    </Layout>
-  )
-}
+const Home: NextPage = ({
+	hero,
+	customers,
+	about,
+	services,
+	technologies,
+	customerFeedback,
+	gallery,
+	footerHero,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+	return (
+		<Layout>
+			<main>
+				<HeroHome heroContent={hero} />
+			</main>
+			<CompaniesSlider customers={customers} />
+			<div className={styles.aboutAndServices}>
+				<About />
+				<OurServices />
+				<div className={styles.diamond}></div>
+			</div>
+			<Technologies />
+			<Portfolio />
+			<CustomerFeedback />
+			<Gallery />
+			<HeroFooter />
+		</Layout>
+	);
+};
 
-export default Home
+export const getStaticProps: GetStaticProps = async (context: any) => {
+	const homeContent: HomeContent = await getHomeContent(context.locale);
+
+	return {
+		props: {
+			...homeContent,
+		},
+	};
+};
+
+export default Home;
