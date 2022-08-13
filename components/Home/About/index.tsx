ab73c76db313faf097166e6f464e10d1d957c46b
Obed Paz <lester.paz@cit.hn>
2022-08-13 10:21:46 -0600

    Add i18n to the about section

--- components/Home/About/index.tsx
+++ components/Home/About/index.tsx
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { useState } from 'react';
+import useTranslation from 'next-translate/useTranslation';
 
 import utils from 'styles/utils.module.scss';
 import styles from './about.module.scss';
 import playIcon from '@/svg/play.svg';
-import GradiantButton from '@/shared/GradiantButton';
+import { GradientButton } from '@/shared/GradientButton';
 import { HomeSections } from 'utils/types/sections.enum';
 
 /* A way to import a component that is not SSR compatible. */
@@ -13,11 +14,12 @@ const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
 
 export default function About() {
   const [showPlaceholder, setShowPlaceholder] = useState<boolean>(true);
+  const { t } = useTranslation('home');
 
   const playVideo = () => setShowPlaceholder(false);
 
   return <section className={styles.aboutSection} id={HomeSections.ABOUT}>
-    <h2 className={`${utils.headingMedium} ${styles.subtitle}`}>About our company</h2>
+    <h2 className={`${utils.headingMedium} ${styles.subtitle}`}>{t('about.title')}</h2>
     <div className={styles.videoContainer}>
     
       {/* The styles for the preview image and play icon can be overridden by targeting the CSS classes react-player__preview, react-player__shadow and react-player__play-icon. */}
@@ -29,10 +31,12 @@ export default function About() {
         height='auto'
       />
       <div className={`${showPlaceholder && styles.mirror}`}></div>
-      {showPlaceholder && <GradiantButton>
-        <Image src={playIcon} alt='Play video'/>
-        Play video
-        </GradiantButton>}
+      {showPlaceholder && 
+        <GradientButton onClick={playVideo}>
+          <Image src={playIcon} alt='Play video'/>
+          <span>{t('about.btn_text')}</span>
+        </GradientButton>
+      }
     </div>
   </section>
 }
