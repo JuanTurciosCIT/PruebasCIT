a28e9a74b90659e5780c6b6a35c27d6be2509944
Obed Paz <lester.paz@cit.hn>
2022-08-16 16:09:13 -0600

    Create a typewriter effect in HeroHome

--- components/Home/HeroHome/index.tsx
+++ components/Home/HeroHome/index.tsx
@@ -1,11 +1,13 @@
 import useTranslation from 'next-translate/useTranslation';
-import { useRouter } from 'next/router';
+import Skeleton from 'react-loading-skeleton';
+import Typewriter from 'typewriter-effect';
 
 import CustomButton from '@/shared/CustomButton';
 import styles from './herohome.module.scss';
 import utils from 'styles/utils.module.scss';
 import { localeNamespaces } from 'utils/types/localeNamespaces.enum';
 import { HeroSection } from 'utils/types/homeContent.interface';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
 
 export default function HeroHome({
 	heroContent,
@@ -13,6 +15,8 @@ export default function HeroHome({
 	heroContent: HeroSection;
 }) {
 	const { t } = useTranslation(localeNamespaces.HOME);
+	const typeWriterText = (heroContent.subtitlePhrasesEN || heroContent.subtitlePhrasesES) as string[];
+	const isMobile = useMediaQuery('(max-width: 428px)');
 
 	return (
 		<section className={styles.hero}>
@@ -22,10 +26,23 @@ export default function HeroHome({
 						{heroContent.titleEN || heroContent.titleES}
 						<span className={styles.creativeText}>CREATIVE</span>
 					</h1>
-					<h2>{heroContent.subtitleEN || heroContent.subtitleES}</h2>
+					<h2>
+						<span>{heroContent.subtitleStartEN || heroContent.subtitleStartES}</span>{isMobile ? <br /> : ' '}
+						<span>{heroContent.subtitleEndES}</span>{' '}
+						<span className={styles.typewriterWrapper}>
+							<Typewriter
+								options={{
+									strings: typeWriterText,
+									autoStart: true,
+									loop: true
+								}} 
+							/>
+						</span>{isMobile && <br />}
+						<span>{heroContent.subtitleEndEN}</span>
+					</h2>
 				</div>
 				<div className={`${utils.textSmall} ${styles.heroDescription}`}>
-					<p>{heroContent.captionES || heroContent.captionEN}</p>
+					<p>{(heroContent.captionES || heroContent.captionEN) || <Skeleton />}</p>
 				</div>
 				<div>
 					<CustomButton>{t('shared.btn_text')}</CustomButton>
