641d8f25732f12791b25e2a49c6c016f83e04641
Obed Paz <lester.paz@cit.hn>
2022-08-18 11:51:12 -0600

    Animate the header component

--- shared/Header/index.tsx
+++ shared/Header/index.tsx
@@ -1,96 +1,135 @@
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import { NextRouter, useRouter } from "next/router";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRef, useState } from 'react';
+import { NextRouter, useRouter } from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
 
-import CreativeLogo from "../../public/svg/creative-logo.svg";
+import CreativeLogo from '../../public/svg/creative-logo.svg';
 import burgerIcon from '../../public/svg/burger-menu.svg';
 import closeIcon from '../../public/svg/close-icon.svg';
-import arrowDownIcon from "../../public/svg/arrow-down.svg";
+import arrowDownIcon from '../../public/svg/arrow-down.svg';
 import styles from './header.module.scss';
 import utils from '../../styles/utils.module.scss';
-import CustomButton from "../CustomButton";
-import MobileMenu from "./MobileMenu";
-import { useMediaQuery } from "../../utils/hooks/useMediaQuery";
-import { NavLink } from "utils/types/navLink.interface";
-import { localeNamespaces } from "utils/types/localeNamespaces.enum";
+import CustomButton from '../CustomButton';
+import MobileMenu from './MobileMenu';
+import { useMediaQuery } from '../../utils/hooks/useMediaQuery';
+import { NavLink } from 'utils/types/navLink.interface';
+import { localeNamespaces } from 'utils/types/localeNamespaces.enum';
+import useOnClickOutside from '@/hooks/useOnClickOutside';
+import { AnimatedContainer } from '../../Animations/AnimatedContainer';
 
 export default function Header({ navLinks }: { navLinks: NavLink[] }) {
-  const router: NextRouter = useRouter();
+	const router: NextRouter = useRouter();
 
-  const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
-  const [mobileMenu, setMobileMenu] = useState<boolean>(false);
-  const [currentLocale, setCurrentLocale] = useState<'English' | 'Español'>(router.locale === 'en' ? 'English' : 'Español');
-  const { t } = useTranslation(localeNamespaces.HOME);
+	const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
+	const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+	const [currentLocale, setCurrentLocale] = useState<'English' | 'Español'>(
+		router.locale === 'en' ? 'English' : 'Español'
+	);
+	const { t } = useTranslation(localeNamespaces.HOME);
 
+	// before 428px viewport width is mobile
+	const isMobile: boolean = useMediaQuery('(max-width: 428px)');
+	const ref = useRef(null);
 
-  // before 428px viewport width is mobile
-  const isMobile: boolean = useMediaQuery('(max-width: 428px)');
+	const toggleDropdownMenu = () => setDropdownMenu(!dropdownMenu);
+	const toggleMobileMenu = () => setMobileMenu(!mobileMenu);
 
-  const toggleDropdownMenu = () => setDropdownMenu(!dropdownMenu);
-  const toggleMobileMenu = () => setMobileMenu(!mobileMenu);
+	const handleClickOutside = () => {
+		if (!dropdownMenu) return;
+		
+		setTimeout(() => {
+			toggleDropdownMenu();
+		}, 500);
+	};
 
-  if (isMobile) {
-    return <>
-      <header className={styles.header}>
-        <div>
-          <Image 
-            src={CreativeLogo}
-            width={148}
-            height={30}
-            alt="Creative Logo"
-          />
-        </div>
+	useOnClickOutside(ref, handleClickOutside);
 
-        {/* onClick => openMobileMenu */}
-        <button className={styles.burgerBtn} onClick={toggleMobileMenu}>
-        <Image 
-            src={mobileMenu ? closeIcon : burgerIcon }
-            alt="burger menu"
-          />
-        </button>
+	if (isMobile) {
+		return (
+			<>
+				<header className={styles.header}>
+					<div>
+						<Image
+							src={CreativeLogo}
+							width={148}
+							height={30}
+							alt='Creative Logo'
+						/>
+					</div>
 
-        {/* Mobile Menu Modal */}
-      </header>
-      { mobileMenu && <MobileMenu navLinks={navLinks} /> }
-    </>
-  }
+					{/* onClick => openMobileMenu */}
+					<button className={styles.burgerBtn} onClick={toggleMobileMenu}>
+						<Image
+							src={mobileMenu ? closeIcon : burgerIcon}
+							alt='burger menu'
+						/>
+					</button>
 
-  return (
-    <>
-      <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image 
-          width={161}
-          height={33}
-          src={CreativeLogo}
-          alt="Creative Logo"
-        />
-      </div>
+					{/* Mobile Menu Modal */}
+				</header>
+				{mobileMenu && <MobileMenu navLinks={navLinks} />}
+			</>
+		);
+	}
 
-      <nav className={styles.navContainer}>
-        <ul className={styles.navMenu}>
-          {
-            navLinks.map((link, index) => (
-              <li className={utils.textSmall} key={link.path+index} ><Link href={link.path} scroll={false}>{t('header.'+link.localeName)}</Link></li>
-            ))
-          }
-          <li className={utils.textSmall}><CustomButton>{t('shared.btn_text')}</CustomButton></li>
-        </ul>
-        <div className={utils.textSmall}>
-          <button className={styles.dropDown} onClick={toggleDropdownMenu}><span>{currentLocale}</span><Image src={arrowDownIcon} alt='Arrow down' /></button>
-        </div>
-      </nav>
+	return (
+		<>
+			<header className={styles.header}>
+				<div className={styles.logo}>
+					<Image
+						width={161}
+						height={33}
+						src={CreativeLogo}
+						alt='Creative Logo'
+					/>
+				</div>
 
-      {/* Dropdown Menu */}
-      {dropdownMenu && (
-        <div className={styles.dropDownMenu}>
-          <button onClick={() => setCurrentLocale('English')}><Link href={router.pathname} locale='en' scroll={false}>English</Link></button>
-          <button onClick={() => setCurrentLocale('Español')}><Link href={router.pathname} locale='es' scroll={false}>Español</Link></button>
-      </div>
-      )}
-    </header>
-    </>
-  )
+				<nav className={styles.navContainer}>
+					<ul className={styles.navMenu}>
+						{navLinks.map((link, index) => (
+							<li className={utils.textSmall} key={link.path + index}>
+								<Link href={link.path} scroll={false}>
+									{t('header.' + link.localeName)}
+								</Link>
+							</li>
+						))}
+						<li className={utils.textSmall}>
+							<CustomButton>{t('shared.btn_text')}</CustomButton>
+						</li>
+					</ul>
+					<div className={utils.textSmall} ref={ref}>
+						<button className={styles.dropDown} onClick={toggleDropdownMenu}>
+							<span>{currentLocale}</span>
+							<Image src={arrowDownIcon} alt='Arrow down' className={`${dropdownMenu ? styles.arrowIconActive : styles.arrowIcon}`} />
+						</button>
+					</div>
+				</nav>
+
+				{/* Dropdown Menu */}
+				{dropdownMenu && (
+					<div className={styles.localeDropDown}>
+						<AnimatedContainer
+							hidden={{ x: '80px', opacity: 0 }}
+							visible={{ x: 0, opacity: 1 }}
+							exit={{ x: '-80px', opacity: 0 }}
+						>
+							<div className={styles.dropDownMenu}>
+								<button onClick={() => setCurrentLocale('English')}>
+									<Link href={router.pathname} locale='en' scroll={false}>
+										English
+									</Link>
+								</button>
+								<button onClick={() => setCurrentLocale('Español')}>
+									<Link href={router.pathname} locale='es' scroll={false}>
+										Español
+									</Link>
+								</button>
+							</div>
+						</AnimatedContainer>
+					</div>
+				)}
+			</header>
+		</>
+	);
 }
