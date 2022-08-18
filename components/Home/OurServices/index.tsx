850b0ee1522611e24216b4e3d5f2b7b6d1ac40bd
Obed Paz <lester.paz@cit.hn>
2022-08-18 11:52:12 -0600

    Add scroll reveal animation to services section

--- components/Home/OurServices/index.tsx
+++ components/Home/OurServices/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
+import { useInView } from 'react-intersection-observer';
 
 import utils from '@/styles/utils.module.scss';
 import styles from './ourservices.module.scss';
@@ -8,47 +9,51 @@ import arrowRight from '@/svg/arrow-right.svg';
 import { HomeSections } from 'utils/types/sections.enum';
 import { ServicesSection } from 'utils/types/homeContent.interface';
 import { localeNamespaces } from 'utils/types/localeNamespaces.enum';
+import { ScrollReveal } from 'Animations/ScrollReveal';
 
 export default function OurServices({
 	services,
 }: {
 	services: ServicesSection[];
 }) {
+	const { inView, entry, ref } = useInView();
 	const { t } = useTranslation(localeNamespaces.HOME);
 
 	return (
-		<section className={styles.servicesSection} id={HomeSections.SERVICES}>
+		<section className={styles.servicesSection} id={HomeSections.SERVICES} ref={ref}>
 			<h2 className={`${utils.headingMedium} ${styles.subtitle}`}>
 				{t('services.title')}
 			</h2>
-			<div className={styles.servicesWrapper}>
-				{services.map((service) => (
-					<div
-						className={styles.serviceCard}
-						key={service.nameEN || service.nameES}
-					>
-						<div className={styles.logo}>
-							<Image
-								src={service.logo}
-								alt={service.nameEN || service.nameES}
-								layout='fill'
-							/>
-						</div>
-						<div className={styles.info}>
-							<h3 className={utils.headingSmall}>
-								{service.nameEN || service.nameES}
-							</h3>
-							<p className={`${utils.textTiny} ${styles.desc}`}>
-								{service.descriptionEN || service.descriptionES}
-							</p>
-							<span className={`${utils.textTiny} ${styles.link}`}>
-								<Link href='/'>{t('services.btn_text')}</Link>
-								<Image src={arrowRight} alt='Arrow right' />
-							</span>
-						</div>
+			<ScrollReveal isVisible={inView}>
+				<div className={styles.servicesWrapper}>
+						{services.map((service) => (
+							<div
+								className={styles.serviceCard}
+								key={service.nameEN || service.nameES}
+							>
+								<div className={styles.logo}>
+									<Image
+										src={service.logo}
+										alt={service.nameEN || service.nameES}
+										layout='fill'
+									/>
+								</div>
+								<div className={styles.info}>
+									<h3 className={utils.headingSmall}>
+										{service.nameEN || service.nameES}
+									</h3>
+									<p className={`${utils.textTiny} ${styles.desc}`}>
+										{service.descriptionEN || service.descriptionES}
+									</p>
+									<span className={`${utils.textTiny} ${styles.link}`}>
+										<Link href='/'>{t('services.btn_text')}</Link>
+										<Image src={arrowRight} alt='Arrow right' />
+									</span>
+								</div>
+							</div>
+						))}
 					</div>
-				))}
-			</div>
+			</ScrollReveal>
 		</section>
 	);
 }
