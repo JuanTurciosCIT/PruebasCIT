4f0eca995b7a69288e0cf3f1578759b6c3f14166
Obed Paz <lester.paz@cit.hn>
2022-08-22 10:52:07 -0600

    Update ScrollReveal component

--- Animations/ScrollReveal/index.tsx
+++ Animations/ScrollReveal/index.tsx
@@ -4,9 +4,10 @@ import { ReactNode } from 'react';
 interface ScrollRevealProps {
   children: ReactNode;
   isVisible: boolean;
+	className?: string;
 }
 
-export const ScrollReveal = ({ children, isVisible } : ScrollRevealProps) => {
+export const ScrollReveal = ({ children, isVisible, className } : ScrollRevealProps) => {
   const animationControl = useAnimation();
 
   if (isVisible) {
@@ -14,13 +15,14 @@ export const ScrollReveal = ({ children, isVisible } : ScrollRevealProps) => {
 			y: 0,
 			opacity: 1,
 			transition: {
-				delay: 0.5
+				delay: 0.3,
+				duration: 0.6
 			}
 		})
 	}
 
 	return (
-    <motion.div initial={{ y: '80px', opacity: 0}} animate={animationControl}>
+    <motion.div className={className} initial={{ y: '110px', opacity: 0}} animate={animationControl}>
       {children}
     </motion.div>
 	);
