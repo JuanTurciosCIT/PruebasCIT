b764eb901450905653c3233d378f61e3abdee750
Obed Paz <lester.paz@cit.hn>
2022-08-22 11:05:42 -0600

    Split technologies section into two components

---
+++ components/Home/Technologies/Atom/index.tsx
@@ -0,0 +1,101 @@
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+
+import citLogo from '@/images/cit_logo.png';
+import styles from './atom.module.scss';
+import { TechnologiesSection } from 'utils/types/homeContent.interface';
+
+interface AtomProps {
+	innerRingTechnologies: Required<TechnologiesSection[]>;
+	middleRingTechnologies: Required<TechnologiesSection[]>;
+	outerRingTechnologies: Required<TechnologiesSection[]>;
+	currentTech: TechnologiesSection;
+	handleTechClick: (tech: TechnologiesSection) => void;
+}
+
+export const TechnologiesAtom = ({
+	innerRingTechnologies,
+	middleRingTechnologies,
+	outerRingTechnologies,
+	currentTech,
+	handleTechClick,
+}: AtomProps) => {
+	return (
+		<motion.div
+			className={styles.atomContainer}
+			drag
+			dragConstraints={{ top: -10, left: -10, bottom: 10, right: 10 }}
+		>
+			<div className={styles.logo}>
+				<Image src={citLogo} alt='CIT Logo' />
+			</div>
+
+			{/* Inner ring */}
+			<div className={styles.innerRing}>
+				{innerRingTechnologies.map((tech, index) => (
+					<button
+						className={styles[`innerTech${index + 1}`]}
+						key={tech.name}
+						onClick={() => handleTechClick(tech)}
+						active-attr={(tech.name == currentTech.name).toString()}
+					>
+						<Image
+							className={styles.logoFilter}
+							src={tech.logo}
+							alt={tech.name}
+							layout='fill'
+							objectFit='contain'
+						/>
+					</button>
+				))}
+			</div>
+
+			{/* Middle Ring */}
+			<div className={styles.midRing}>
+				{middleRingTechnologies.map((tech, index) => (
+					<button
+						className={styles[`middleTech${index + 1}`]}
+						key={tech.name}
+						onClick={() => handleTechClick(tech)}
+						active-attr={(tech.name == currentTech.name).toString()}
+					>
+						<Image
+							className={styles.logoFilter}
+							src={tech.logo}
+							alt={tech.name}
+							layout='fill'
+							objectFit='contain'
+						/>
+					</button>
+				))}
+
+				{/* dots */}
+				<div className={styles.dot1}></div>
+				<div className={styles.dot2}></div>
+				<div className={styles.dot3}></div>
+				<div className={styles.dot4}></div>
+				<div className={styles.dot5}></div>
+			</div>
+
+			{/* Outer ring */}
+			<div className={styles.largerRing}>
+				{outerRingTechnologies.map((tech, index) => (
+					<button
+						className={styles[`largeTech${index + 1}`]}
+						key={tech.name}
+						onClick={() => handleTechClick(tech)}
+						active-attr={(tech.name == currentTech.name).toString()}
+					>
+						<Image
+							className={styles.logoFilter}
+							src={tech.logo}
+							alt={tech.name}
+							layout='fill'
+							objectFit='contain'
+						/>
+					</button>
+				))}
+			</div>
+		</motion.div>
+	);
+};
