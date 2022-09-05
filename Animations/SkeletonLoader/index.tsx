cf9f2b77293cc0e45b6fd2abc5c4a5ff7dba8719
Obed Paz <lester.paz@cit.hn>
2022-09-05 09:20:45 -0600

    Fix skeleton loader

--- Animations/SkeletonLoader/index.tsx
+++ Animations/SkeletonLoader/index.tsx
@@ -1,8 +1,13 @@
+import React, { useId } from 'react';
+// import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import ContentLoader from 'react-content-loader';
+// const ContentLoader = dynamic(() => import('react-content-loader'), { ssr: false });
 import styles from './skeleton.module.scss';
 import { useLockBodyScroll } from '@/hooks/useLockBodyScroll';
 
-export const SkeletonLoader = (props: any) => {
+export const SkeletonLoader = () => {
+	React.useLayoutEffect = React.useEffect;
+	const id = useId();
 	useLockBodyScroll();
 
 	return (
@@ -13,7 +18,7 @@ export const SkeletonLoader = (props: any) => {
         viewBox={'0 0 1440 800'}
         backgroundColor="#232323"
         foregroundColor="#2F2F2F"
-        {...props}
+				uniqueKey={id}
       >
 				<rect x='20' y='57' rx='4' ry='4' width='1440' height='84' />
 				<rect x='217' y='157' rx='4' ry='4' width='1024' height='291' />
@@ -27,4 +32,20 @@ export const SkeletonLoader = (props: any) => {
 			</ContentLoader>
 		</div>
 	);
+
+	// return (
+	// 	<div className={styles.skeleton}>
+	// 		<SkeletonTheme baseColor="#232323" highlightColor="#2F2F2F">
+	// 			<Skeleton height={84} />
+	// 			<Skeleton height={291} />
+	// 			<Skeleton height={25} />
+	// 			<Skeleton circle={true} height={40} width={40} />
+	// 			<Skeleton circle={true} height={45} width={45} />
+	// 			<Skeleton circle={true} height={50} width={50} />
+	// 			<Skeleton circle={true} height={45} width={45} />
+	// 			<Skeleton circle={true} height={40} width={40} />
+	// 			<Skeleton height={25} />
+	// 		</SkeletonTheme>
+	// 	</div>
+	// )
 };
