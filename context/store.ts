331bbac2e9e661832dec21b9ae52f73dc56ca68f
Obed Paz <lester.paz@cit.hn>
2022-09-08 10:52:59 -0600

    Add redux config

---
+++ context/store.ts
@@ -0,0 +1,38 @@
+import { Action, configureStore, createSlice, PayloadAction, ThunkAction } from "@reduxjs/toolkit";
+import { FooterSection } from "utils/types/homeContent.interface";
+
+// slices
+export const footerContentSlice = createSlice({
+  name: "footerContent",
+  initialState: {
+    contact: {},
+    locations: {},
+  } as FooterSection,
+  reducers: {
+    setFooterContent: (state, action: PayloadAction<FooterSection>) => {
+      state.contact = action.payload.contact;
+      state.locations = action.payload.locations;
+    },
+  },
+});
+
+const store = () => configureStore({
+  reducer: {
+    [footerContentSlice.name]: footerContentSlice.reducer,
+  }
+})
+
+export default store;
+export const footerContentActions = footerContentSlice.actions;
+
+// export type RootState = ReturnType<typeof store.getState>
+// export type AppDispatch = typeof store.dispatch
+
+export type AppStore = ReturnType<typeof store>;
+export type AppState = ReturnType<AppStore["getState"]>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action
+>;
