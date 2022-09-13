2426e0155f979c4a066db4b0af567887f56623b1
Obed Paz <lester.paz@cit.hn>
2022-09-13 15:34:11 -0600

    Split CaseStudies component

--- components/CaseStudies/PaginatedCaseStudies/CaseStudyPreview/index.tsx
+++ components/CaseStudies/PaginatedCaseStudies/CaseStudyPreview/index.tsx
@@ -1,28 +1,28 @@
 import Image from 'next/image';
 import utils from '/styles/utils.module.scss';
-import picture1 from '/public/images/business-chart-visual.jpg';
+import useTranslation from 'next-translate/useTranslation';
 import CustomButton from '@/shared/CustomButton';
 
 import styles from './caseStudyPreview.module.scss';
+import { CaseStudy } from 'utils/types/caseStudies.interface';
 
-export const CaseStudyPreview = () => {
+export const CaseStudyPreview = ({ caseStudy }: { caseStudy: CaseStudy }) => {
+	const { t } = useTranslation('caseStudies');
+
 	return (
 		<div className={styles.CScontainer}>
 			<div className={styles.pictureWrapper}>
-				<Image className={styles.picture} src={picture1} alt='' />
+				<Image quality={100} className={styles.picture} src={caseStudy.picture} alt={caseStudy.titleEN || caseStudy.titleES} layout='fill' objectFit='contain' />
 			</div>
 			<div className={styles.descriptionContainer}>
 				<h2 className={`${utils.headingMedium} ${styles.CStitle}`}>
-					Boosting cows health and well-being
+					{caseStudy.titleEN || caseStudy.titleES}
 				</h2>
 				<p className={`${utils.textMedium} ${styles.descriptionText}`}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat.
+					{caseStudy.descriptionEN || caseStudy.descriptionES}
 				</p>
 				<div className={styles.button}>
-					<CustomButton>See Case</CustomButton>
+					<CustomButton>{t('caseStudies.btn_text')}</CustomButton>
 				</div>
 			</div>
 		</div>
