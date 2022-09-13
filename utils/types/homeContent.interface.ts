f6e295ef7679dd23d17d33fcbcf16ce2f48a7c99
Obed Paz <lester.paz@cit.hn>
2022-09-13 15:21:50 -0600

    Update common interfaces

--- utils/types/homeContent.interface.ts
+++ utils/types/homeContent.interface.ts
@@ -1,3 +1,5 @@
+import { FooterHeroSection, FooterSection } from "./commonContent.interface";
+
 export interface HomeContent {
   hero: HeroSection;
   customers: CustomersSection[];
@@ -24,6 +26,8 @@ export interface HeroSection {
   captionEN?: string;
   captionES?: string;
   backgroundImage?: string;
+  linkedPage: string;
+  isVisible: boolean;
 }
 export interface CustomersSection {
   title?: string;
@@ -102,30 +106,4 @@ export interface GallerySection {
   images: string[];
   captionES?: string;
   captionEN?: string;
-}
-
-export interface FooterHeroSection {
-  titleES?: string;
-  titleEN?: string;
-  captionES?: string;
-  captionEN?: string;
-  backgroundImg?: string;
-}
-
-export interface LocationInterface {
-  address: string;
-  isVisible: boolean;
-}
-
-export interface Contact {
-  facebook: string;
-  instagram: string;
-  linkedin: string;
-  email: string;
-  phone: string;
-}
-
-export interface FooterSection {
-  locations: LocationInterface[];
-  contact: Contact;
 }
