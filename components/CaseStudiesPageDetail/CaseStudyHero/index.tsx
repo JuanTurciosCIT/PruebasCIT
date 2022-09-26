3f44e720d3702b3dd55695d8dee5c8b32611c5b3
Obed Paz <lester.paz@cit.hn>
2022-09-26 14:24:31 -0600

    Fetch dynamic data for hero in case study detail page

--- components/CaseStudiesPageDetail/CaseStudyHero/index.tsx
+++ components/CaseStudiesPageDetail/CaseStudyHero/index.tsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
-import useTranslation from 'next-translate/useTranslation';
 import Image from 'next/image';
 
-import { CustomButton } from '@/shared/CustomButton';
 import styles from './CaseStudyHero.module.scss';
 import utils from 'styles/utils.module.scss';
-import { localeNamespaces } from 'utils/types/localeNamespaces.enum';
-import { HeroSection } from 'utils/types/homeContent.interface';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
-import bipbipLogo from '/public/images/logo-bipbip-white.png'
 
-export const CaseStudyHero = () => {
-	// const { t } = useTranslation(localeNamespaces.HOME);
+interface CaseStudyHeroProps {
+  heroTitleEN?: string;
+  heroTitleES?: string;
+  heroDescriptionEN?: string;
+  heroDescriptionES?: string;
+  pdf: string;
+  heroImage: string;
+}
+
+export const CaseStudyHero = ({ heroContent }: { heroContent: CaseStudyHeroProps }) => {
 	const isMobile = useMediaQuery('(max-width: 428px)');
 
 	return (
@@ -20,22 +22,23 @@ export const CaseStudyHero = () => {
 				<div className={styles.infoContainer}>
           <div className={`${utils.headingMedium} ${styles.titlesWrapper}`}>
             <h1 className={styles.title}>
-              {/* {heroContent.titleEN || heroContent.titleES} */}
-              Delivery Startup exceeds internal and external users expectations with top of the line Custom Solution
+              {heroContent.heroTitleEN || heroContent.heroTitleES}
             </h1>
-            <p className={`${utils.textMedium} ${styles.desc}`}>Our end-to-end solution development partnership with the largest restaurant chain in Honduras.</p>
+            <p className={`${utils.textMedium} ${styles.desc}`}>
+              {heroContent.heroDescriptionEN || heroContent.heroDescriptionES}
+            </p>
               <div className={styles.btnWrapper}>
               <a
                 className={`${styles.downloadBtn} ${styles.textMedium}`}
                 download
-                href="/files/example.pdf"
-                target="_self"
+                href={heroContent.pdf}
+                target="_blank"
                 rel="noopener noreferrer"
               >Download PDF</a>
               </div>
           </div>
           <div className={styles.logoContainer}>
-            <Image src={bipbipLogo} alt='logo' layout='fill' objectFit='contain'  />
+            <Image src={heroContent.heroImage} alt='Case Study Logo' layout='fill' objectFit='contain'  />
           </div>
         </div>
 			</div>
