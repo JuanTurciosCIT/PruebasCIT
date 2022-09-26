3f6c9dfe467e65d29e646b7a37e3504d958cc5fb
Obed Paz <lester.paz@cit.hn>
2022-09-26 14:40:10 -0600

    Fetch data from api for case study metrics

--- components/CaseStudiesPageDetail/Metrics/index.tsx
+++ components/CaseStudiesPageDetail/Metrics/index.tsx
@@ -1,36 +1,46 @@
 import CountUp from 'react-countup';
+import { useMemo } from 'react';
 
 import styles from './caseStudyMetrics.module.scss';
 import utils from 'styles/utils.module.scss';
+import { CaseStudyMetricsInterface } from 'utils/types/caseStudy.interface';
 
-export const CaseStudyMetrics = () => {
+export const CaseStudyMetrics = ({
+	metrics,
+}: {
+	metrics: CaseStudyMetricsInterface[];
+}) => {
 
-  return <section className={styles.section}>
-    <div className={styles.wrapper}>
-      <div className={styles.card}>
-        <h3 className={`${utils.headingMedium} ${styles.title}`}><CountUp end={10} duration={1} enableScrollSpy scrollSpyOnce /> Month</h3>
-        <p className={`${utils.textMedium} ${styles.desc}`}>Initial Development of MVP</p>
-      </div>
-      <div className={styles.card}>
-        <h3 className={`${utils.headingMedium} ${styles.title}`}><CountUp end={350_000} duration={1.5} enableScrollSpy scrollSpyOnce />+</h3>
-        <p className={`${utils.textMedium} ${styles.desc}`}>Downloads of the APP </p>
-      </div>
-      <div className={styles.card}>
-        <h3 className={`${utils.headingMedium} ${styles.title}`}><CountUp end={200_000} duration={1.4} enableScrollSpy scrollSpyOnce />+</h3>
-        <p className={`${utils.textMedium} ${styles.desc}`}>Customers Fully Registered</p>
-      </div>
-      <div className={styles.card}>
-        <h3 className={`${utils.headingMedium} ${styles.title}`}><CountUp end={400} duration={1} enableScrollSpy scrollSpyOnce />+</h3>
-        <p className={`${utils.textMedium} ${styles.desc}`}>Delivery Drivers Managed </p>
-      </div>
-      <div className={styles.card}>
-        <h3 className={`${utils.headingMedium} ${styles.title}`}><CountUp end={1.5} decimal={'.'} decimals={1} duration={0.4} enableScrollSpy scrollSpyOnce />+ Million</h3>
-        <p className={`${utils.textMedium} ${styles.desc}`}>Orders processed and delivered </p>
-      </div>
-      <div className={styles.card}>
-        <h3 className={`${utils.headingMedium} ${styles.title}`}><CountUp end={100} duration={0.6} enableScrollSpy scrollSpyOnce />+</h3>
-        <p className={`${utils.textMedium} ${styles.desc}`}>Admin users from 15 different departments</p>
-      </div>
-    </div>
-  </section>
-}
+	const formatValue = (value: number) => {
+		return value % 2 === 0 ? String(value.toLocaleString('en-US')) : value.toLocaleString('en-US');
+	}
+
+	const memoizedValue = useMemo(() => formatValue, []);
+
+	return (
+		<section className={styles.section}>
+			<div className={styles.wrapper}>
+				{metrics.map((metric) => (
+					<div className={styles.card} key={metric.id}>
+						<h3 className={`${utils.headingMedium} ${styles.title}`}>
+							<CountUp
+								end={metric.metricValue}
+								duration={1}
+								enableScrollSpy
+								scrollSpyOnce
+								decimals={1}
+								decimal="."
+								separator=","
+								formattingFn={memoizedValue}
+							/>{' '}
+							{metric.suffixEN || metric.suffixES}
+						</h3>
+						<p className={`${utils.textMedium} ${styles.desc}`}>
+							{metric.descriptionEN || metric.descriptionES}
+						</p>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+};
