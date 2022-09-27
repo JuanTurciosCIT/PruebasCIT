9b802cda8dcef0e4294b68c30c31482e06e062e2
Obed Paz <lester.paz@cit.hn>
2022-09-27 14:30:05 -0600

    Add a 500 error page

---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+export default function Custom500() {
+  return <div style={{width: '100vw', height: '100vh', display: 'grid', placeContent: 'center'}}>
+    <h1 style={{color: 'white'}}>This page is under construction</h1>
+    <button>Return home</button>
+    <button>Return back</button>
+  </div>
+}
