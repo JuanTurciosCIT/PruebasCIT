cdf572e07bc3eac36f328ccfcba8a16037193f75
Obed Paz <lester.paz@cit.hn>
2022-09-27 14:31:43 -0600

    Update Case Study detail page

--- pages/caseStudies/[csid].tsx
+++ pages/caseStudies/[csid].tsx
@@ -5,6 +5,7 @@ import {
 	NextPage,
 } from 'next';
 import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
 
 // Components
 import { Layout } from '@/shared/Layout';
@@ -13,33 +14,55 @@ import { CaseStudyMetrics } from 'components/CaseStudiesPageDetail/Metrics';
 import { GeneralInfo } from 'components/CaseStudiesPageDetail/GeneralInfo';
 import { Achievements } from 'components/CaseStudiesPageDetail/Achievements';
 import { AppStack } from 'components/CaseStudiesPageDetail/AppStack';
-// import { OurServices } from 'components/Home/OurServices';
+import { DeliveredServices } from 'components/CaseStudiesPageDetail/DeliveredServices';
 import { HeroFooter } from '@/shared/HeroFooter';
 
 // Services
 import { CaseStudiesContentService } from 'utils/services/CaseStudiesContentService';
-import { getCaseStudyContent } from 'utils/services';
+import { getCaseStudyDetailContent } from 'utils/services';
 import { OurProcess } from 'components/CaseStudiesPageDetail/OurProcess';
+import { DetailCaseStudyPage } from 'utils/types/caseStudy.interface';
+import { CaseStudyCategory } from 'utils/types/caseStudies.interface';
+import { TechnologiesSection } from 'utils/types/commonContent.interface';
+import { CaseStudyGallery } from 'components/CaseStudiesPageDetail/CaseStudyGallery';
 
-export const getStaticPaths: GetStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
 	const caseStudies = await CaseStudiesContentService.getCaseStudies('en');
 
-	const paths = caseStudies.map((caseStudy) => {
-		return {
-			params: { csid: String(caseStudy.id) },
-		};
-	});
+	// const paths: any = [];
+	const paths: any = [
+		{
+			params: { csid: String(caseStudies[0].id) },
+			locale: locales?.[0],
+		},
+		{
+			params: { csid: String(caseStudies[0].id) },
+			locale: locales?.[1],
+		},
+	];
+	// caseStudies.map((caseStudy) => {
+	// 	paths.push({
+	// 		params: { csid: String(caseStudy.id) }, locale: locales?.[0]
+	// 	});
+	// 	paths.push({
+	// 		params: { csid: String(caseStudy.id) }, locale: locales?.[1]
+	// 	});
+	// });
 
 	return {
 		paths,
-		fallback: true,
+		fallback: false,
 	};
 };
 
 export const getStaticProps: GetStaticProps = async (
 	context: GetStaticPropsContext
 ) => {
-	const caseStudyContent = await getCaseStudyContent(context.locale);
+	const csid = context.params?.csid;
+	const caseStudyContent = await getCaseStudyDetailContent(
+		context.locale,
+		Number(csid)
+	);
 
 	return {
 		props: {
@@ -47,43 +70,53 @@ export const getStaticProps: GetStaticProps = async (
 			...caseStudyContent,
 		},
 		revalidate: 10,
+		notFound: !caseStudyContent,
 	};
 };
 
-const CaseStudyPageDetail: NextPage<any> = ({ services, footerHero, footer }) => {
-	const router = useRouter();
-	const { csid } = router.query; // csid is the case study id
+const CaseStudyPageDetail: NextPage<DetailCaseStudyPage> = ({
+	technologies,
+	caseStudyInfo,
+	achievements,
+	howItWorks,
+	categories,
+	detailContent,
+	metrics,
+	services,
+	ourProcess,
+	gallery,
+	footerHero,
+	footer,
+}) => {
+	const heroContent = {
+		heroTitleEN: detailContent.heroTitleEN,
+		heroTitleES: detailContent.heroTitleES,
+		heroDescriptionEN: detailContent.heroDescriptionEN,
+		heroDescriptionES: detailContent.heroDescriptionES,
+		pdf: detailContent.pdf,
+		heroImage: caseStudyInfo.logo,
+	};
 
-	if (router.isFallback) {
-		return (
-			<div
-				style={{
-					fontSize: '32px',
-					color: '#fff',
-					width: '100vw',
-					height: '100vh',
-					display: 'grid',
-					placeContent: 'center',
-				}}
-			>
-				Loading...
-			</div>
-		);
-	}
+	const atGlanceContent = {
+		industry: categories as CaseStudyCategory[],
+		companySize:
+			detailContent.companySizeEN || (detailContent.companySizeES as string),
+		goal: detailContent.goalEN || (detailContent.goalES as string),
+		technologies: technologies as TechnologiesSection[],
+	};
 
 	return (
 		<Layout footerContent={footer}>
-			<CaseStudyHero />
-      <CaseStudyMetrics />
-			<section style={{backgroundColor: '#232323', paddingTop: '36px'}}>
-				<GeneralInfo />
-				<Achievements />
+			<CaseStudyHero heroContent={heroContent} />
+			<CaseStudyMetrics metrics={metrics} />
+			<section style={{ backgroundColor: '#232323', paddingTop: '36px' }}>
+				<GeneralInfo content={atGlanceContent} />
+				<Achievements achievements={achievements} />
 			</section>
-			<AppStack />
-			<div style={{paddingInline: '34px'}}>
-				{/* <OurServices services={services} /> */}
-			</div>
-			<OurProcess />
+			<AppStack appStack={howItWorks.appStack} image={howItWorks.image} />
+			<DeliveredServices services={services} />
+			<OurProcess content={ourProcess} />
+			<CaseStudyGallery content={gallery} />
 			<HeroFooter content={footerHero} />
 		</Layout>
 	);
