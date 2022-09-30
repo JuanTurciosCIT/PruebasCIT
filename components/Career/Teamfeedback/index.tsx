a2d2c47c4b59fa3802b484f20a0c6046335fca58
Obed Paz <lester.paz@cit.hn>
2022-09-30 09:44:43 -0600

    Create slider feedback in Career page

---
+++ components/Career/Teamfeedback/index.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/image';
+import { useRef, useCallback } from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import useTranslation from 'next-translate/useTranslation';
+import { SwiperOptions } from 'swiper';
+import { useInView } from 'react-intersection-observer';
+import 'swiper/css';
+
+import utils from '@/styles/utils.module.scss';
+import styles from './teamFeedback.module.scss';
+import SliderButtons from '@/shared/SliderButtons';
+import { useMediaQuery } from 'utils/hooks/useMediaQuery';
+import { CustomerFeedbackSection } from 'utils/types/homeContent.interface';
+import { localeNamespaces } from 'utils/types/localeNamespaces.enum';
+import { ScrollReveal } from 'Animations/ScrollReveal';
+
+export const TeamFeedback = ({
+	feedback,
+}: {
+	feedback: CustomerFeedbackSection[];
+}) => {
+	const isMobile: boolean = useMediaQuery('(max-width: 428px)');
+	const { ref, inView } = useInView();
+	const { t } = useTranslation(localeNamespaces.HOME);
+
+	const swiper = useSwiper();
+	const swiperRef = useRef(swiper);
+
+	const swiperOptions: SwiperOptions = {
+		spaceBetween: 20,
+		slidesPerView: isMobile ? 1 : 3,
+		loop: true,
+		touchMoveStopPropagation: isMobile ? true : false,
+		rewind: true,
+		centeredSlides: true,
+	};
+
+	/* A React hook that is used to memoize a function. */
+	const nextSlide = useCallback(() => swiperRef.current.slideNext(), []);
+	const prevSlide = useCallback(() => swiperRef.current.slidePrev(), []);
+
+	return (
+		<section className={styles.section} ref={ref}>
+			<h2 className={`${utils.headingMedium} ${styles.subtitle}`}>
+				{t('customerFeedback.title')}
+        What is like being part of creative
+			</h2>
+			<ScrollReveal isVisible={inView} className={styles.container}>
+				<Swiper
+					{...swiperOptions}
+					onSwiper={(swiper) => {
+						swiperRef.current = swiper;
+					}}
+				>
+					{feedback.map((item) => (
+						<SwiperSlide key={item.id}>
+							<div className={styles.feedbackCard}>
+								<div className={styles.customerPic}>
+									<Image
+										priority
+										quality={100}
+										src={item.picture}
+										alt='Customer'
+										width={80}
+										height={80}
+										objectFit='cover'
+										placeholder='blur'
+										blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAQAAABuBnYAAAAAEUlEQVR42mNcPpEBBTAOjAAA3qIJybv4Wl8AAAAASUVORK5CYII='
+									/>
+								</div>
+								<div className={styles.info}>
+									<h3 className={`${utils.headingMedium} ${styles.name}`}>
+										{item.name}
+									</h3>
+                  <p className={`${styles.role} ${styles.comment}`}>Web Developer</p>
+									<p className={styles.comment}>
+										{item.commentEN || item.commentES}
+									</p>
+								</div>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</ScrollReveal>
+
+			<SliderButtons next={nextSlide} prev={prevSlide} />
+		</section>
+	);
+}
