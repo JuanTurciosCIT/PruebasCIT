143eb38050a85912db338da31ccfc4e04cccbc0d
Obed Paz <lester.paz@cit.hn>
2022-10-03 09:49:22 -0600

    Update custom btn component

--- shared/CustomButton/index.tsx
+++ shared/CustomButton/index.tsx
@@ -1,13 +1,13 @@
-import { memo } from 'react';
+import { HTMLAttributeAnchorTarget, memo } from 'react';
 import Link from 'next/link';
 
 import styles from './customButton.module.scss';
 
-export const CustomButton = memo(function CustomButton({ children, path, as }: { path?: string, as?: string, children: React.ReactNode }) 
+export const CustomButton = memo(function CustomButton({ children, path, target }: { path?: string, target?: HTMLAttributeAnchorTarget, children: React.ReactNode }) 
 {
   return <div className={styles.btnWrapper}>
-    <Link href={path || ''} as={as} scroll={true}>
-      <button className={styles.customBtn}>{children}</button>
+    <Link href={path || ''} scroll={true}>
+      <a className={styles.customBtn} target={target}>{children}</a>
     </Link>
   </div>
 });
