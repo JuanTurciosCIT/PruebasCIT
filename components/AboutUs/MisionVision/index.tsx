94e137176d4f794d45c1a5d327f9ad13d16c0a75
Obed Paz <lester.paz@cit.hn>
2022-10-05 08:39:13 -0600

    Update MisionVision component

--- components/AboutUs/MisionVision/index.tsx
+++ components/AboutUs/MisionVision/index.tsx
@@ -2,24 +2,31 @@ import styles from "./misionVision.module.scss";
 import misionImage from 'public/images/Mision-min.jpg'
 import Image from "next/image";
 
+
+const OUR_MISSION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus ex in lacus commodo, non rhoncus lorem egestas. In posuere erat sit amet turpis pulvinar rhoncus. Aenean fringilla fermentum efficitur. 
+
+Nulla facilisi. Ut et sem gravida, pellentesque velit ut, auctor lectus. Aliquam sit amet convallis magna, id molestie lacus. Proin molestie lorem id vulputate cursus.`;
+
 export const MisionVisionComponent = () => {
+  const splittedMission = OUR_MISSION.split('\n\n');
+
   return (
     <section className={styles.section}>
       <div className={styles.wrapper}>
-        <div>
+        <div className={styles.flex}>
             <div className={styles.content}>
-                <h1>Our mission</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                    tempus ex in lacus commodo, non rhoncus lorem egestas. In posuere
-                    erat sit amet turpis pulvinar rhoncus. Aenean fringilla fermentum
-                    efficitur. Nulla facilisi. Ut et sem gravida, pellentesque velit ut,
-                    auctor lectus. Aliquam sit amet convallis magna, id molestie lacus.
-                    Proin molestie lorem id vulputate cursus.
-                </p>
+                <h3>Our mission</h3>
+                {
+                  splittedMission.map((paragraph, index) => <p key={paragraph}>{paragraph}</p>)
+                }
+                <ul className={styles.list}>
+                    <li>Lorem ipsum dolor.</li>
+                    <li>Lorem ipsum dolor.</li>
+                    <li>Lorem ipsum dolor.</li>
+                </ul>
             </div>
             <div className={styles.image}>
-                <Image  src={misionImage} alt='A people team' />
+                <Image src={misionImage} alt='A people team' layout="fill" objectFit="cover" />
             </div>
         </div>
       </div>
