43e6cc093f05dcf0b9ec6a94dfdc0a03a78d6832
Gustavo Meza <gustavo.meza@cit.hn>
2022-10-13 17:11:39 -0600

    Nombre de empresa

--- components/Home/CustomerFeedback/index.tsx
+++ components/Home/CustomerFeedback/index.tsx
@@ -68,6 +68,7 @@ export const CustomerFeedback = ({
 										blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAQAAABuBnYAAAAAEUlEQVR42mNcPpEBBTAOjAAA3qIJybv4Wl8AAAAASUVORK5CYII='
 									/>
 								</div>
+                {/* Card info*/}
 								<div className={styles.info}>
 									<div className={styles.stars}>
 										<Image
@@ -106,9 +107,14 @@ export const CustomerFeedback = ({
 											height={14}
 										/>
 									</div>
-									<h3 className={`${utils.headingMedium} ${styles.name}`}>
-										{item.name}
-									</h3>
+
+                  <section className={styles.infoHeader} >
+                    <h3 className={`${utils.headingMedium} ${styles.name}`}>
+                      {item.name}
+                    </h3>
+                    <p  className={styles.captionName}>Creative Information Technologies</p>
+                  </section>
+
 									<p className={styles.comment}>
 										{item.commentEN || item.commentES}
 									</p>
