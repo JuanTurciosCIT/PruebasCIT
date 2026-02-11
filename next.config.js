733c87ac9b3ce64d229144233952c6df17cab94a
JuanTurciosCIT <juan.turcios@cit.hn>
2026-02-11 14:45:39 -0600

    Amplify4

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 const nextTranslate = require('next-translate');
-const locale = process.env.NEXT_LOCALE;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 module.exports = nextTranslate({
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
   images: {
+    unoptimized: true,
     minimumCacheTTL: 31536000,
-    domains: supabaseUrl ? [new URL(supabaseUrl).hostname] : []
+    domains: supabaseUrl ? [new URL(supabaseUrl).hostname] : [],
   },
   experimental: {
     esmExternals: false,
-  }
+  },
 });
